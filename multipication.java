27334d787ddddedef82c45759f19ed2b49b485b2
subhamsupriyamohapatra <subhammohapatra633@gmail.com>
2023-06-04 21:07:28 +0530

    multiplication

---
+++ multipication.java
@@ -0,0 +1,9 @@
+public class multipication{
+ public static void main(String arg[]){
+   int a,b,c;
+         a = 10;
+         b = 20;
+    	 c = a*b;
+	 System.out.println("multiplication is :"+c);
+}
+}
